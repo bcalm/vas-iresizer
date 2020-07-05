fd5a2668c362cf68cd7c2b5046045af5ee983d46
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-07-05 09:46:28 +0530

    Accepting buffer

--- worker.js
+++ worker.js
@@ -1,12 +1,12 @@
 const redis = require('redis');
-const redisClient = redis.createClient();
-const { get, completeProcessing } = require('imageSets.js');
-const { saveImage, getImage } = require('./connectWithFileSystem');
-const { resizeImage } = require('./resizeImage');
+const redisClient = redis.createClient({ db: 1 });
+const { get, completeProcessing } = require('./lib/imageSets.js');
+const { saveImage, getImage } = require('./lib/connectWithFileSystem');
+const { resizeImage } = require('./lib/resizeImage');
 
 const getJob = () => {
   return new Promise((resolve, reject) => {
-    redisClient.blpop('resizeQueue', 1, (err, res) => {
+    redisClient.blpop('resize_queue', 1, (err, res) => {
       console.log(res);
       if (res) resolve(res[1]);
       else reject('no job');
@@ -18,9 +18,8 @@ const runLoop = () => {
   getJob()
     .then((id) => {
       get(redisClient, id)
-        .then((details) => {
-          const jobDetails = JSON.parse(details);
-          getImage(jobDetails.fileName)
+        .then((jobDetails) => {
+          getImage(jobDetails.file_name)
             .then(resizeImage)
             .then(saveImage)
             .then((fileName) => completeProcessing(redisClient, id, fileName));
